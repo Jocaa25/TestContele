80e688b5a92c6830b22f7243d6139f964506be78
Jocaa25 <joca.ribeiro25@gmail.com>
2023-12-06 14:16:48 -0300

    Pessoal, eu nao consegui efetuar a conexção com o banco no docker e nem com um banco local criado por mim. o banco do Docker eu consigo acessar apenas via terminal. O banco que eu criei eu consigo acessar pelo azure studio, porem nao consegui com a aplicação. {  "type": "internal server error (6ad29608-c2e4-4627-815f-6f904a3e67f8)",  "message": "connect ECONNREFUSED 127.0.0.1:3307",  "details": {   "errno": -111,   "code": "ECONNREFUSED",   "syscall": "connect",   "address": "127.0.0.1",   "port": 3307,   "fatal": true  } }

--- src/modules/common/handlers/knex/knex.js
+++ src/modules/common/handlers/knex/knex.js
@@ -1,24 +1,41 @@
 'use strict'
 
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      host: process.env.WRITER_MYSQL_HOST,
-      user: process.env.WRITER_MYSQL_USER,
-      port: '3306',
-      password: process.env.WRITER_MYSQL_PASS,
-      database: 'main'
-  },
-  pool: {
-    min: 1,
-    max: 4,
-  },
+// const knex = require('knex')({
+//     client: 'mysql',
+//     connection: {
+//       host: process.env.WRITER_MYSQL_HOST,
+//       user: process.env.WRITER_MYSQL_USER,
+//       port: "3307",
+//       password: process.env.WRITER_MYSQL_PASS,
+//       database: 'main'
+//   },
+//   pool: {
+//     min: 1,
+//     max: 4,
+//   },
+// });
+
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'admin',
+    database : 'main',
+    port: '3307'
+  }
 });
+
+
 
 const getTransaction = async () => {
 
     const transaction = await knex.transaction()
 
+    conso
+    
+    le.log(knex);
     return {transaction};
 }
 
